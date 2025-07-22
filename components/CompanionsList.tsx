41a558f347e0eca6229821e08a2ffc8e4a9db969
lazermagma <masterabdelrahman7@gmail.com>
2025-07-22 16:55:51 +0300

    implement Recent Sessions Table

--- components/CompanionsList.tsx
+++ components/CompanionsList.tsx
@@ -1,10 +1,54 @@
+import {
+    Table,
+    TableBody,
+    TableCaption,
+    TableCell,
+    TableHead,
+    TableHeader,
+    TableRow,
+} from "@/components/ui/table"
+import {cn} from "@/lib/utils";
+import Link from "next/link";
 
+interface CompanionListProps{
+    title: string;
+    companions?:Companion[];
+    classNames?:string;
+}
 
-const CompanionsList = () => {
+const CompanionsList = ({title,companions,classNames}:CompanionListProps) => {
     return (
-        <div>
-            Companion List
-        </div>
+        <article className={cn('companion-list', classNames)}>
+            <h2 className="font-bold text-3xl">Recent Sessions</h2>
+            <Table>
+
+                <TableHeader>
+                    <TableRow>
+                        <TableHead className="text-lg w-2/3">Lessons</TableHead>
+                        <TableHead className="text-lg">Subject</TableHead>
+                        <TableHead className="text-lg text-right">Duration</TableHead>
+
+                    </TableRow>
+                </TableHeader>
+
+                <TableBody>
+
+                    {companions?.map(({id,subject, name, duration}) => (
+                        <TableRow key={id}>
+                            <TableCell>
+                                <Link href={`/companions/${id}`}>
+                                    {subject}
+                                </Link>
+                            </TableCell>
+                        </TableRow>
+                    ))}
+
+
+
+
+                </TableBody>
+            </Table>
+        </article>
     );
 };
 
