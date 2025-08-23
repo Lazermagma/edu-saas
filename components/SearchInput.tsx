795ef2bee0859af59276c6a2067a107721af14ea
lazermagma <masterabdelrahman7@gmail.com>
2025-08-23 04:30:17 +0300

    Add: Search Input & Subject Filter

---
+++ components/SearchInput.tsx
@@ -0,0 +1,52 @@
+'use client';
+
+import {usePathname, useSearchParams ,useRouter} from "next/navigation";
+import {useEffect, useState} from "react";
+import Image from "next/image";
+import {formUrlQuery, removeKeysFromUrlQuery} from "@jsmastery/utils";
+
+const SearchInput = () => {
+    const router = useRouter();
+    const pathname = usePathname();
+    const searchParams = useSearchParams();
+    const query = searchParams.get('topic') || '';
+
+    const [searchQuery, setSearchQuery] = useState('');
+
+    useEffect(() => {
+
+        const delayDebounceFn = setTimeout (()=>{
+            if(searchQuery){
+                const newUrl = formUrlQuery({
+                    params: searchParams.toString(),
+                    key: "topic",
+                    value: searchQuery,
+                });
+
+                router.push(newUrl ,{scroll : false});
+            } else {
+                if ( pathname === '/companions'){
+                    const newUrl = removeKeysFromUrlQuery({
+                        params: searchParams.toString(),
+                        keysToRemove: ["topic"],
+                    });
+
+                    router.push(newUrl, { scroll: false });
+                }
+            }
+        } , 500)
+
+    }, [searchQuery,router,searchParams ,pathname]);
+    return (
+        <div className="relative border border-black rounded-lg items-center gap-2 flex px-2 py-1 h-fit">
+           <Image src="/icons/search.svg" alt="search" width={15} height={15}/>
+            <input placeholder="Search Companions..."
+                   className="outline-none"
+                   value={searchQuery}
+                   onChange={e=>setSearchQuery(e.target.value)}
+            />
+        </div>
+    );
+};
+
+export default SearchInput;
